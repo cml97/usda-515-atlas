a9267d17a7ea2360f20ae51cf3bdb10ccd0dae14
cml97 <clederberg@greysteel.com>
2026-09-11 11:05:13 -0400

    Add files via upload

--- docs/assets/map.js
+++ docs/assets/map.js
@@ -58,8 +58,8 @@
   Atlas.load().then(({ meta }) => {
     map = L.map("map", { preferCanvas: true }).setView([38.5, -96], 4);
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-      attribution: '&copy; OpenStreetMap contributors, &copy; CARTO',
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 18,
     }).addTo(map);
 
